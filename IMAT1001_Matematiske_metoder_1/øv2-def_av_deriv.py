ec67d75d369a30ba05fa6fd85ac227c917f7c430
raflemakt <raflemakt@hotmail.com>
2021-09-26 12:51:17 +0200

    Fixed duplicate

--- IMAT1001_Matematiske_metoder_1/øv2-def_av_deriv.py
+++ IMAT1001_Matematiske_metoder_1/øv2-def_av_deriv.py
@@ -1,22 +1,10 @@
-import numpy as np
+h = 1e-8
 
-A = np.zeros((5,5))
-for i in range(5):
-    for j in range(5):
-        A[i, j] = (i + 1)**(4 - j)
+def f(x):
+    return (1 + (1/x))**x
 
-b = np.array([2,0,1,0,0])
+def derive(func, x):
+    return (func(x + h) - func(x))/ h
 
-# Finner x_2
-sol = np.linalg.solve(A, b)
-print("x_2 = " + str(round(sol[1], 3)))
-
-# Bytter ut kolonne 2 med b
-A_2b = A
-for row in range(5):
-    for col in range(5):
-        A_2b[row][1] = b[row]
-
-detA_2b = np.linalg.det(A_2b)
-print(A_2b)
-print("detA_2(b) = " + str(round(detA_2b, 2)))
+d = derive(f, 0.1)
+print(d)
