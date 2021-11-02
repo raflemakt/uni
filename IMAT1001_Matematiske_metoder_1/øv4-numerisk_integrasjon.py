1fc59febccc77396e9e787e6a479a0d4e97aa2ae
raflemakt <raflemakt@hotmail.com>
2021-11-02 11:27:58 +0100

    Improved numerical methods

--- IMAT1001_Matematiske_metoder_1/øv4-numerisk_integrasjon.py
+++ IMAT1001_Matematiske_metoder_1/øv4-numerisk_integrasjon.py
@@ -4,27 +4,27 @@ def midtpkt(func, a, b, N=10, resolution=6):
     num_integral = 0
     delta_x = (b - a)/N
     for n in range(N):
-        x = a + (n * delta_x)
-        x_bar = func(x)
-        print(n, x_bar)
-        num_integral += round(x_bar, resolution)
+        x_bar = a + (n * delta_x) + delta_x/2
+        term = func(x_bar)
+        print(n, term)
+        num_integral += round(term, resolution)
     return num_integral * (delta_x)
 
 
-def trapesmetode(func, a, b, N=10, resolution=6):
+def trapes(func, a, b, N=10, resolution=6):
     print(f"Trapesmetoden: funksjon: {func} intervall: {a} -- {b}, steg: {N}")
     func = eval("lambda x: " + func)
     num_integral = 0
     delta_x = (b - a)/N
-    for n in range(1, N+1):
-        if n == 1 or n == N:
+    for n in range(0, N+1):
+        if n == 0 or n == N:
             koef = 1
         else:
             koef = 2
         x = a + (n * delta_x)
-        x_bar = koef * func(x)
-        print(n, x_bar)
-        num_integral += round(x_bar, resolution)
+        term = koef * func(x)
+        print(n, term)
+        num_integral += round(term, resolution)
     result = num_integral * (delta_x) / 2
     return round(result, resolution)
 
@@ -34,17 +34,17 @@ def simpsons(func, a, b, N=10, resolution=6):
     func = eval("lambda x: " + func)
     num_integral = 0
     delta_x = (b - a)/N
-    for n in range(1, N+1):
-        if n == 1 or n == N:
+    for n in range(0, N+1):
+        if n == 0 or n == N:
             koef = 1
-        elif n%2 == 0:
-            koef = 4
         elif (n + 1)%2 == 0:
+            koef = 4
+        elif n%2 == 0:
             koef = 2
         x = a + (n * delta_x)
-        x_bar = koef * func(x)
-        print(n, x_bar)
-        num_integral += round(x_bar, resolution)
+        term = koef * func(x)
+        print(n, term)
+        num_integral += round(term, resolution)
     result = num_integral * (delta_x) / 3
     return round(result, resolution)
 
@@ -53,5 +53,5 @@ def simpsons(func, a, b, N=10, resolution=6):
 # Careful, eval() is dangerous. Do not use in production
 func = "(x**3 - 1)**0.5"
 print("result is", midtpkt(func, 1, 2, N=10), "\n")
-print("result is", trapesmetode(func, 1, 2, N=10), "\n")
+print("result is", trapes(func, 1, 2, N=10), "\n")
 print("result is", simpsons(func, 1, 2, N=10), "\n")
